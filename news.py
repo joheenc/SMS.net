0aeede4ec78cdd778d9e14481cad7aa542b3854b
Vincent Huang <vinhuang848@gmail.com>
2019-10-20 01:14:46 -0400

    Created a news search via SMS

---
+++ news.py
@@ -0,0 +1,46 @@
+from newsapi import NewsApiClient
+import requests
+import json
+import urllib.request as urllib
+import re
+
+# Init
+newsapi = NewsApiClient(api_key = 'c9e9c07ecedc48efb0998ab457f74bf4')
+
+def getNews(query):
+    userdata = {'q':query, 'apiKey': 'c9e9c07ecedc48efb0998ab457f74bf4'}
+    reponse = requests.get('https://newsapi.org/v2/top-headlines?', params=userdata)
+    reponse = reponse.json()
+
+    searchResults = []
+    for i in range(3):
+        searchResults.append({"title" : reponse['articles'][i]["title"],
+            "source" : reponse['articles'][i]['source']['name'],
+            "preview" : reponse['articles'][i]["description"],
+            "url" : reponse['articles'][i]["url"]})
+
+
+def urlToParagraphs(url):
+    try:
+        headers = {}
+        headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+        req = urllib.Request(url, headers=headers)
+        resp = urllib.urlopen(req)
+        respData = resp.read()
+        paragraphs = re.findall(r'<p>(.*?)</p>', str(respData))
+        parsed = []
+
+        for para in paragraphs:
+            a = re.sub(r'\<[^>]*\>', '', para)
+            b = re.sub(r'&#9.*?93;', '', a)
+            b = re.sub(r'\\', '', b)
+            c  = b.replace('\\n', '')
+            parsed.append(c)
+        return parsed
+
+    except Exception as e:
+        print(str(e))
+        return []
+
+#result = getNews('bitcoin')
+#result =urlToParagraphs(#insert user input)
